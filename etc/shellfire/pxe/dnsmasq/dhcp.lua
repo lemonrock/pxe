1c016f77ed2d153571236bfd88ac1c3a62b28296
Raphael Cohn <raphael.cohn@stormmq.com>
2016-06-02 19:31:44 +0100

    Initial commit

---
+++ etc/shellfire/pxe/dnsmasq/dhcp.lua
@@ -0,0 +1,28 @@
+-- This file is part of pxe. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/pxe/master/COPYRIGHT. No part of pxe, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+-- Copyright © 2014-2015 The developers of pxe. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/pxe/master/COPYRIGHT.
+
+
+function init() {
+}
+
+function lease(actionString, tagValuePairsTable) {
+}
+
+function shutdown() {
+}
+
+function tftp(table) {
+	--table.destination_address;
+	--table.file_name;
+	--table.file_size;
+}
+
+function arp(table) {
+	--table.mac_addres; -- sic
+	--table.client_address;
+}
+
+function arp-old(table) {
+	--table.mac_addres; -- sic
+	--table.client_address;
+}
